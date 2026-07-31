3bc8566feac585f6b03134ede162df009c875703
OssaBellator <141500316+OssaBellator@users.noreply.github.com>
2026-07-31 15:46:43 +1000

    Include persistent deletion implementation

--- crates/ucof-experiments/src/immutable_successor.rs
+++ crates/ucof-experiments/src/immutable_successor.rs
@@ -13,6 +13,7 @@ include!("immutable_successor/part5.rs");
 include!("immutable_successor/batch.rs");
 include!("immutable_successor/persistent_batch.rs");
 include!("immutable_successor/persistent_insert.rs");
+include!("immutable_successor/persistent_delete.rs");
 include!("immutable_successor/rewrite.rs");
 
 #[allow(clippy::len_without_is_empty)]
