2f713118e983fc347b321d9a65ea4a43913359c0
OssaBellator <141500316+OssaBellator@users.noreply.github.com>
2026-08-01 14:39:35 +1000

    Wire inventory format error conversion

--- crates/ucof-experiments/src/immutable_successor.rs
+++ crates/ucof-experiments/src/immutable_successor.rs
@@ -27,6 +27,7 @@ mod source_api {
     include!("immutable_successor/source.rs");
     include!("immutable_successor/source_full.rs");
     include!("immutable_successor/source_inventory.rs");
+    include!("immutable_successor/source_inventory_conversion.rs");
 }
 
 pub use source_api::*;
